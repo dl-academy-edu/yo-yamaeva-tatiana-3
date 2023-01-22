d749e968f8337ae1cc1b2d10fc24ea005cc84231
taaanyya <tanyayamaeva456@gmail.com>
2023-01-22 15:38:26 +0400

    commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let age = 16;
+
+let bigint = 1838449n;
+
+let eat = "tomato";
+
+let goToSchool = false;
+
+let name = null;
+
+let walk;
+
+let user = {};
+
+let id = Symbol("id");
+
+
+console.log(Number(age), String(age), Boolean(age));
+console.log(String(1838449n), Number(1838449n), Boolean(1838449n));
+console.log(String(eat), Number(eat), Boolean(eat));
+console.log(Boolean(goToSchool), String(goToSchool), Number(goToSchool));
+console.log(Number(name), String(name), Boolean(name));
+console.log(Number(walk), String(walk), Boolean(walk));
+console.log(Number(user), String(user), Boolean(user));
+// console.log(Number(id), id.toString(), Boolean(id));
+
