cd4b1663ce24e928141f9c435cd71aca5590fe53
taaanyya <tanyayamaeva456@gmail.com>
2023-01-30 15:24:25 +0400

    commit

--- script2.js
+++ script2.js
@@ -1,19 +1,14 @@
 // 1 задание
 
-    let num = 1
-    let numb = 80
-    userNumber = prompt("Введите число");
 
-    while(1) {
-            userNumber = prompt("Введите число");
-        if (1 < 80) {
-        console.log(1<80)
-        break;
-        }
-        else {
-            userNumber = prompt("Введите число");
-        }
-    }
+    let number = prompt("Введите число");
+    console.log(number++)
+
+
+ 
+
+
+
 
     // let bankBalance = 1500;
     // let amount = 2099;
@@ -36,14 +31,4 @@
 
 
 
-// for (let i=1; i <= 15; i++) {
-//     console.log(i%4)
-// }
-
-
-// 2 задание
-
-// let i = 6
-
-// let userNumber = prompt("Введите число");
 
