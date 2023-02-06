9957d386c5a462df872ccd0d7fef7aa44aaa9b7d
taaanyya <tanyayamaeva456@gmail.com>
2023-02-06 21:30:30 +0400

    commit

--- script2.js
+++ script2.js
@@ -10,15 +10,67 @@
 
 // 2 задание
 
-let result = num;
 let number = prompt("Введите число");
+let i = number;
+let result = factorial;
 
-for (let i = number; i > 0; factorial)
-if (result < 1){
-let number = prompt("Введите число");
-} else {
-    while(number > 1) {
-        number--;
-        result *= num;
-    }
+if (i > 1) {
+while(i > 1) {
+    result = result * (i-1);
+    i--;
 }
+} else {
+    let number = prompt("Введите число");
+}
+
+
+
+// let number = 5;
+// let numberFactorial = number;
+
+//     while(number > 1){
+//         numberFactorial = numberFactorial * (number-1);
+//         number--;
+//     }
+// console.log(numberFactorial);
+
+
+
+
+
+
+
+
+
+
+
+// let result = num;
+// let number = prompt("Введите число");
+
+// for (let i = number; i > 0; factorial)
+// if (result < 1){
+// let number = prompt("Введите число");
+// } else {
+//     while(number > 1) {
+//         number--;
+//         result *= num;
+//     }
+// }
+
+// Подсчёт без остановки
+
+// let number = prompt("Введите число");
+// let i = number;
+// let result = 1;
+
+// if (i > result) {
+// while(number > 1) {
+//     i = i * (i-1);
+//     number--;
+//     console.log(i);
+//     // console.log(result);
+//     // result *= i + 1;
+// }
+// } else {
+//     let number = prompt("Введите число");
+// }
